83180a1731a91c9af7aa6009c15315300898fcc1
kyubuns <jp@ex.cx>
2014-10-15 22:19:03 +0900

    JumpSceneButton 追加

---
+++ NGUI/JumpSceneButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpSceneButton : MonoBehaviour
+{
+  [SerializeField] private string sceneName;
+
+  void OnClick()
+  {
+    Application.LoadLevel(sceneName);
+  }
+}
