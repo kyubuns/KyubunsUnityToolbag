27ed3474b3674c15627f37bd203d4383c9abaae0
kyubuns <jp@ex.cx>
2014-10-19 20:39:48 +0900

    DebugExtensions作成
    なんかもうちょっといい感じにならないかなぁ

---
+++ Extensions/DebugExtensions.cs
@@ -0,0 +1,6 @@
+﻿public static class DebugExtensions
+{
+  public static void Log(string message, params object[] args) { UnityEngine.Debug.Log(string.Format(message, args)); }
+  public static void LogError(string message, params object[] args) { UnityEngine.Debug.LogError(string.Format(message, args)); }
+  public static void LogWarning(string message, params object[] args) { UnityEngine.Debug.LogWarning(string.Format(message, args)); }
+}
