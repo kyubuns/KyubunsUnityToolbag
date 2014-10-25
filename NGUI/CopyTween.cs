82c2dd244d5e14488663a901ac135db6df074ba6
kyubuns <jp@ex.cx>
2014-10-25 13:34:59 +0900

    CopyTween追加

---
+++ NGUI/CopyTween.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class CopyTween : MonoBehaviour
+{
+  [SerializeField] private GameObject original;
+  [SerializeField] private float delay;
+
+  void Awake()
+  {
+    if(!original)
+    {
+      Debug.LogWarning("missing original game object.");
+      return;
+    }
+    MoveComponents();
+    Destroy(this);
+  }
+
+  void MoveComponents()
+  {
+    var from = original;
+    var to = gameObject;
+    var components = from.GetComponents<UITweener>();
+    foreach(var component in components)
+    {
+      Type type = component.GetType();
+      var copy = (UITweener)to.AddComponent(type);
+      var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+      foreach (var field in fields) field.SetValue(copy, field.GetValue(component));
+      copy.delay = delay;
+      copy.Sample(0.0f, false);
+    }
+  }
+}
